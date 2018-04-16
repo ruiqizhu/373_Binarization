0c04502e02fff87f683ced8c68cc34c6942cc728
Connor Fitzgibbon <cfitzgib@andrew.cmu.edu>
2018-04-16 18:48:31 -0400

    Added directory depth searching with input folder

--- combined.py
+++ combined.py
@@ -37,82 +37,81 @@ def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
 
 if __name__ == '__main__':
-    folder = sys.argv[1] # image file name
-    for filename in os.listdir(os.path.join(os.getcwd(), folder)):
-        print(filename)
-        if(filename[-4:] == ".tif"):
-            img_name = folder + '/' + filename
-            threshold = .4
-
-            #Resize the image
-            size = 648 , 486
-            im = Image.open(img_name)
-            im.thumbnail(size, Image.ANTIALIAS)
-            im.save(img_name[:-4] + "_resized.png")
-            original = mpimg.imread(img_name[:-4] + "_resized.png")
-            #print(original)
-            #Binarize the image if not already greyscale
-            if(isinstance(original[0][0], np.ndarray)):
-                gray = rgb2gray(original)
-            else:
-                gray = original
-            #print(gray)
-            height = len(gray)
-            width = len(gray[0])
-            for row in range(0, height):
-                for col in range(0, width):
-                    if gray[row][col] > threshold:
-                        gray[row][col] = 1 # turn the pixel white
-                    else:
-                        gray[row][col] = 0 # turn the pixel black
-
-
+    folder = sys.argv[1] #top level of directory where images are
+    #go through directory
+    for subdir, dirs, files in os.walk(os.path.join(os.getcwd(), folder)):
+        for filename in files:
             
+            if(filename[-4:] == ".tif"):
+                img_name = subdir + '/' + filename
+                threshold = .4
+                print(img_name)
 
-            #Create a structuring element and filter the image
-            selem = disk(25)
-            img = closing(img_as_ubyte(gray), selem)
-            
+                #Resize the image
+                size = 648 , 486
+                im = Image.open(img_name)
+                im.thumbnail(size, Image.ANTIALIAS)
+                im.save(img_name[:-4] + "_resized.png")
+                original = mpimg.imread(img_name[:-4] + "_resized.png")
+                #print(original)
+                #Binarize the image if not already greyscale
+                if(isinstance(original[0][0], np.ndarray)):
+                    gray = rgb2gray(original)
+                else:
+                    gray = original
+                #print(gray)
+                height = len(gray)
+                width = len(gray[0])
+                for row in range(0, height):
+                    for col in range(0, width):
+                        if gray[row][col] > threshold:
+                            gray[row][col] = 1 # turn the pixel white
+                        else:
+                            gray[row][col] = 0 # turn the pixel black
 
-            #Read in the filtered image and label it
-            img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)[1]  # ensure binary
-            img = cv2.bitwise_not(img)
-            connectivity = 100
-            output = cv2.connectedComponentsWithStats(img, connectivity, cv2.CV_32S)
-            binary_map = (img > 0).astype(np.uint8)
 
-            #The first cell is the number of identified components
-            num_labels = output[0]
-            # The second cell is the label matrix
-            labels = output[1]
-            # The third cell is the stat matrix
-            stats = output[2]
-            # The fourth cell is the centroid matrix
-            centroids = output[3]
+                
+
+                #Create a structuring element and filter the image
+                selem = disk(25)
+                img = closing(img_as_ubyte(gray), selem)
+                
+
+                #Read in the filtered image and label it
+                img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)[1]  # ensure binary
+                img = cv2.bitwise_not(img)
+                connectivity = 100
+                output = cv2.connectedComponentsWithStats(img, connectivity, cv2.CV_32S)
+                binary_map = (img > 0).astype(np.uint8)
+
+                #The first cell is the number of identified components
+                num_labels = output[0]
+                # The second cell is the label matrix
+                labels = output[1]
+                # The third cell is the stat matrix
+                stats = output[2]
+                # The fourth cell is the centroid matrix
+                centroids = output[3]
 
 
-            # Map component labels to hue val
-            label_hue = np.uint8(179*labels/np.max(labels))
-            blank_ch = 255*np.ones_like(label_hue)
-            labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
+                # Map component labels to hue val
+                label_hue = np.uint8(179*labels/np.max(labels))
+                blank_ch = 255*np.ones_like(label_hue)
+                labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
 
-            # cvt to BGR for display
-            labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
+                # cvt to BGR for display
+                labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
 
-            # set bg label to black
-            labeled_img[label_hue==0] = 255
+                # set bg label to black
+                labeled_img[label_hue==0] = 255
 
-            print("Num labels: " + str(num_labels))
-            print("Stats: " + str(stats))
-            print("Centroids: " + str(centroids))
-
-            #save labeled image
-            #cv2.imshow('labeled', labeled_img)
-            cv2.imwrite(img_name[:-4] + "_labeled.png", labeled_img)
-            # cv2.setMouseCallback("labeled", click_oocyte)
-            # cv2.waitKey()
-            with open(img_name[:-4] + '_area.csv', 'w', newline='') as csvfile:
-                areawriter = csv.writer(csvfile, delimiter=' ',
-                                        quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                for i in range(1, len(stats)):
-                    areawriter.writerow([np.array2string(stats[i][4]) + "," + np.array2string(centroids[i][0]) + "," + np.array2string(centroids[i][1])] )
+                #save labeled image
+                #cv2.imshow('labeled', labeled_img)
+                cv2.imwrite(img_name[:-4] + "_labeled.png", labeled_img)
+                # cv2.setMouseCallback("labeled", click_oocyte)
+                # cv2.waitKey()
+                with open(img_name[:-4] + '_area.csv', 'w', newline='') as csvfile:
+                    areawriter = csv.writer(csvfile, delimiter=' ',
+                                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                    for i in range(1, len(stats)):
+                        areawriter.writerow([np.array2string(stats[i][4]) + "," + np.array2string(centroids[i][0]) + "," + np.array2string(centroids[i][1])] )
